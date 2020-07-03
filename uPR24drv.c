beb7edf360fab46a399e3ef0a2a3a2a1b8815d59
Anton Pavlov <anton.pavlov@gmail.com>
2020-07-03 19:16:39 -0400

    Cleanup

--- uPR24drv.c
+++ uPR24drv.c
@@ -1,7 +1,6 @@
-
-
 /*
- * uPR24drv - uPR24drv legacy prototype
+ * uPR24drv
+ * https://tnsense.com
  */
 
 #define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
@@ -9,44 +8,49 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
-/* Device major umber */
+/* Device major number 
+ * https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html#majors-and-minors
+ */
 static int major;
 
-/*
- * Methods
- */
+/* Functions */
 
+/* Transfer data from the device into the user space */
 static ssize_t uPR24drv_read(struct file *filp,
 			   char __user *buf, size_t count, loff_t *ppos)
 {
-	pr_info("return EOF\n");
+	pr_info("uPR24drv_read call\n");
 
 	return 0;
 }
 
+/* Transfer data from the user space into the device */
 static ssize_t uPR24drv_write(struct file *filp,
 			    const char __user *buf, size_t count, loff_t *ppos)
 {
-	pr_info("got %ld bytes\n", count);
+	pr_info("uPR24drv_write call, %ld bytes\n", count);
 
 	*ppos += count;	
 	return count;
 }
 
+/* https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html#open-and-release */
 static int uPR24drv_open(struct inode *inode, struct file *filp)
 {
-	pr_info("uPR24drv opened\n");
+	pr_info("uPR24drv_open call\n");
 
 	return 0;
 }
 
 static int uPR24drv_release(struct inode *inode, struct file *filp)
 {
-	pr_info("uPR24drv released\n");
+	pr_info("uPR24drv_release call\n");
 
 	return 0;
 }
 
+/* Allowed system calls */
+/* https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html#struct-file-operations */
 static struct file_operations uPR24drv_fops = {
 	.owner		= THIS_MODULE,
 	.read		= uPR24drv_read,
@@ -55,21 +59,18 @@ static struct file_operations uPR24drv_fops = {
 	.release	= uPR24drv_release
 };
 
-/*
- * Module stuff
- */
-
+/* Module */
 static int __init uPR24drv_init(void)
 {
 	int ret;
 
 	ret = register_chrdev(0, "uPR24drv", &uPR24drv_fops);
 	if (ret < 0) {
-		pr_err("unable to register char device! Error %d\n", ret);
+		pr_err("Failure to register a char device! Error code: %d\n", ret);
 		return ret;
 	}
 	major = ret;
-	pr_info("got major %d\n", major);
+	pr_info("Major: %d\n", major);
 
 	return 0;
 }
